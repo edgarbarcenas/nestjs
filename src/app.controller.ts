b5eb29d331dc534130c3d12184ae6920f989398e
Edgar Barcenas <barcenas.edgar@seiza-ti.cl>
2022-05-27 18:26:04 -0400

    Implementacion de servicios y controladores modulares

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hola mundo!';
+  }
+
+  @Get('nuevo')
+  newEnpoint() {
+    return 'Yo soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'con /sas/';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './constrollers/products.controller';
+import { CategoriesController } from './constrollers/categories.controller';
+import { CustomerController } from './constrollers/customer.controller';
+import { UserController } from './constrollers/user.controller';
+import { OrderController } from './constrollers/order.controller';
+import { BrandController } from './constrollers/brand.controller';
+import { ProductsService } from './services/products.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    ProductsController,
+    CategoriesController,
+    CustomerController,
+    UserController,
+    OrderController,
+    BrandController,
+  ],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
